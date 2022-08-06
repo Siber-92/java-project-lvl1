978a41a392bc45ad826415acb65729f592b06b7a
Rostislav <sibercat.92@gmail.com>
2022-08-06 17:58:39 +0300

    fix liner errors

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,8 +2,7 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Welcome to the Brain Games!" +
-                "\nMay I have your name? ");
+        System.out.print("Welcome to the Brain Games!" + "\nMay I have your name? ");
 
         String playerName = Cli.getPlayerInput();
 
