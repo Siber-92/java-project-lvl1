bc03b6410f700c9069ddf32a750d9f49507e4988
Rostislav <sibercat.92@gmail.com>
2022-08-08 15:29:28 +0300

    Refactor the games class GCD

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -9,26 +9,16 @@ public class GCD {
         firstOperand = Math.abs(firstOperand);
         secondOperand = Math.abs(secondOperand);
 
-        int greatestOperand = Math.max(firstOperand, secondOperand);
         int leastOperand = Math.min(firstOperand, secondOperand);
 
-        if (greatestOperand % leastOperand == 0) {
-            return String.valueOf(leastOperand);
-        }
-
-        int firstDivisor = 1;
-        int secondDivisor = 1;
-        int divisorOfBothOperand = 1;
-
-        for (int i = 2; i <= firstOperand / 2; i++) {
-            firstDivisor = firstOperand % i == 0 ? i : firstDivisor;
-            for (int j = 2; j <= secondOperand / 2; j++) {
-                secondDivisor = secondOperand % j == 0 ? j : secondDivisor;
-                divisorOfBothOperand = firstDivisor == secondDivisor ? firstDivisor : divisorOfBothOperand;
+        while (leastOperand > 0) {
+            if (firstOperand % leastOperand == 0 && secondOperand % leastOperand == 0) {
+                break;
             }
+            leastOperand--;
         }
 
-        return String.valueOf(divisorOfBothOperand);
+        return String.valueOf(leastOperand);
     }
 
     public static String generateGameQuest(int firstOperand, int secondOperand) {
